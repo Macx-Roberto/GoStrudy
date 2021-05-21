ce71d4703aa4c190c15c306008d636de3a9f0009
Macx-Roberto <macxizinhocesconetto@hotmail.com>
2021-05-20 22:58:24 -0300

    - corrigido problemas / implemtado leitura dos sites e escrita do log em arquivos

--- src/hello.go
+++ src/hello.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
+
+const _repeticoes int = 1
+const _tempoPausa int = 1
+const _prefixSite string = "Site:"
+const _msgSucesso string = " foi carregado com sucesso!"
+const _msgErro string = " está com problemas. Status Code:"
 
 func main() {
 	for {
@@ -13,7 +24,7 @@ func main() {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -34,18 +45,81 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 	return comandoLido
 }
 
 func iniciarMonitoramento() {
-	fmt.Println("Monitorando...")
-	site := "https://soundcloud.com/"
-	resp, _ := http.Get(site)
-	fmt.Println(resp)
-	// '_' serve para ignporar um dos retornos da funcao
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
-	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	sites := retornarSitesArquivo()
+	// sites
+	// if sites.1 <= 0 {
+	// 	os.Exit(0)
+	// }
+
+	for i := 0; i <= _repeticoes; i++ {
+		for j, site := range sites {
+			fmt.Println("Testando site: ", j+1, " - ", site)
+			testarSite(site)
+		}
+		// time.Sleep(100)
 	}
+}
+
+func retornarSitesArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+	arquivo.Close()
+	return sites
+}
+
+func testarSite(site string) {
+
+	var cMensagem string
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("erro")
+	}
+
+	if resp.StatusCode == 200 {
+		cMensagem = _prefixSite + site + _msgSucesso
+		fmt.Println(cMensagem)
+		registraLog(cMensagem)
+	} else {
+		cMensagem = _prefixSite + site + _msgErro + err.Error()
+		fmt.Println(cMensagem)
+		registraLog(cMensagem)
+	}
+}
+
+func registraLog(log string) {
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao salvar o log:", err)
+	}
+	log = time.Now().Format("02/01/2006 15:04:05") + " | " + log + "\n"
+	arquivo.WriteString(log)
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	fmt.Println("Exibindo Logs...")
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(arquivo))
 }
